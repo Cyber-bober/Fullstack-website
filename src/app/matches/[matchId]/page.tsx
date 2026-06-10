9e70a0d64c8fb0281d730113c62dd267d2233335
Vladislav <bykovvlados@vk.com>
2026-06-10 15:07:27 +0400

    не получилось не фортануло

--- src/app/matches/[matchId]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function MatchPage(props: any) {
-  const id = props.params?.matchId;
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Матч {id}</h1>
-    </div>
-  );
-}
